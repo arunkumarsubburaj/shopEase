b0f778fe031cc201201530ab4e5b22b367b58396
arunkumarsubburaj <arun123.sa@gmail.com>
2023-12-05 01:57:01 +0530

    Completed Base Template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,39 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import {
+  NavigationStart,
+  Router,
+  RouterLink,
+  RouterLinkActive,
+  RouterOutlet,
+} from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'shopEase';
+  constructor(router: Router) {
+    router.events
+      .pipe(filter((e) => e instanceof NavigationStart))
+      .subscribe((e) => {
+        const menuIcon = document.getElementById('menu-icon') as HTMLDivElement;
+        menuIcon.classList.remove('show');
+        const navList = document.querySelector('.nav-list') as HTMLElement;
+        navList.classList.remove('show');
+      });
+  }
+  ngAfterViewInit() {}
+  toggleIcon() {
+    (document.getElementById('menu-icon') as HTMLDivElement).classList.toggle(
+      'show'
+    );
+    const navList = document.querySelector('.nav-list') as HTMLElement;
+    navList.classList.toggle('show');
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { UnderConstructionComponent } from './under-construction/under-construction.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
+  { path: 'products', loadChildren: () => import('./products/products.module').then(m => m.ProductsModule) },
+  {path: "**", component: UnderConstructionComponent}
+  // Other routes
+];
